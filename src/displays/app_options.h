1fbaeb951eca05bb9d75ee55bbfd259516356c35
Kent Louise Tonino <tonino.kentlouise@gmail.com>
2024-07-13 13:38:09 +0800

    Show the options.

---
+++ src/displays/app_options.h
@@ -0,0 +1,7 @@
+#ifndef APP_OPTIONS_H
+#define APP_OPTIONS_H
+#include "./app_options.c"
+
+extern void app_options(void);
+
+#endif
--- src/main.c
+++ src/main.c
@@ -1,4 +1,5 @@
 #include "./displays/app_description.h"
+#include "./displays/app_options.h"
 #include "./displays/clear_terminal.h"
 #include "./displays/escape_sequence.h"
 #include <stdio.h>
@@ -11,6 +12,11 @@ int main(void) {
 
   // Show CLI tool description.
   app_description();
+  add_new_line();
+  add_new_line();
+
+  // Show CLI tool options.
+  app_options();
   add_new_line();
   add_new_line();
 
