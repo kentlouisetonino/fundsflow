cc8edb8dd44ac3354605e7c17267a5e73fc1ac3d
Kent Louise Tonino <tonino.kentlouise@gmail.com>
2024-07-13 13:31:52 +0800

    App description.

---
+++ src/displays/escape_sequence.c
@@ -0,0 +1,4 @@
+#include <stdio.h>
+
+void add_new_line(void) { printf("\n"); }
+void add_new_tab(void) { printf("\t"); }
--- src/main.c
+++ src/main.c
@@ -1,7 +1,18 @@
+#include "./displays/app_description.h"
+#include "./displays/clear_terminal.h"
+#include "./displays/escape_sequence.h"
 #include <stdio.h>
 
 int main(void) {
-  printf("Hello world.");
+  // Clear the terminal.
+  clear_terminal();
+  add_new_line();
+  add_new_line();
+
+  // Show CLI tool description.
+  app_description();
+  add_new_line();
+  add_new_line();
 
   return 0;
 }
